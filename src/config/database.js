b584782555a6b8aeaf1578690dea53488c4dd4ce
G MONTANHER <gabrielmontanherr@gmail.com>
2022-09-29 19:47:10 -0300

    Updates

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@
 module.exports = {
     JWT_SECRET: "wq21212121212",
     dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: '1996',
-    database: 'crud-node',
+    host: 'jbb8y3dri1ywovy2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    username: 'myq3ar7jy0ndxgui',
+    password: 'geibocjmnnyg6r6o',
+    database: 'lm495oomsxjlhliq',
     define: {
         timestamp:  true,
         underscored: true
